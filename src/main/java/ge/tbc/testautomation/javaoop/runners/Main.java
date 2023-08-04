fb38110d26f403a0d5ce26caa33d38dafb690658
giorgi gengashvili <giorgi.gengashvili0@gmail.com>
2023-08-04 23:32:49 +0400

    finish annotationsAndComparataros homework

--- src/main/java/ge/tbc/testautomation/javaoop/runners/Main.java
+++ src/main/java/ge/tbc/testautomation/javaoop/runners/Main.java
@@ -1,40 +1,78 @@
 package ge.tbc.testautomation.javaoop.runners;
-import ge.tbc.testautomation.exceptionsStringOperationsRegex.LimitException;
-import ge.tbc.testautomation.exceptionsStringOperationsRegex.RadiusException;
-import ge.tbc.testautomation.javaoop.figures.Circle;
-//import ge.tbc.testautomation.javaoop.figures.Rectangle;
-//import ge.tbc.testautomation.javaoop.figures.Triangle;
-//import ge.tbc.testautomation.javaoop.util.HelperFunctions;
-//import ge.tbc.testautomation.javaoop.util.Util;
-import java.util.Random;
+import ge.tbc.testautomation.annotationsAndComparators.Analyzable;
+import ge.tbc.testautomation.annotationsAndComparators.Rectangle;
+import ge.tbc.testautomation.annotationsAndComparators.RectangleComparator;
+import ge.tbc.testautomation.annotationsAndComparators.VariableNameAnnotation;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 
 public class Main {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
 
-        Circle circle = new Circle(10.0);
+        Analyzable analyzable = new Analyzable();
 
-        System.out.println("circle area - " + circle.getArea());
-        System.out.println("circle length - " + circle.getLength());
-        circle.printPackageName();
+        Field[] fields = analyzable.getClass().getDeclaredFields();
+
+        for (Field field : fields) {
+            VariableNameAnnotation annotation = field.getAnnotation(VariableNameAnnotation.class);
+
+            if(!(field.getName().equalsIgnoreCase(annotation.name()))) {
+                System.out.println("VARIABLE NAME MISMATCH FOUND.");
+                System.out.println(annotation.name() + " != " + field.getName() + "\n");
+            }
+        }
+
+        String string1 = "string1";
+        String string2 = "string2";
+        String string3 = "string3";
+        String string4 = "string4";
+
+
+        ArrayList<Integer> integerArrayList = new ArrayList<>(Arrays.asList(10,3,2,9,4,5,1,6,8,7));
+
+        System.out.println("unsorted integerArrayList: " + integerArrayList);
+
+        Collections.sort(integerArrayList);
+
+        System.out.println("sorted integerArrayList: " + integerArrayList);
+
+
+
+        List<Rectangle> rectangleList = new ArrayList<>(
+                Arrays.asList(
+                        new Rectangle(4,5),
+                        new Rectangle(9,6),
+                        new Rectangle(2,8),
+                        new Rectangle(1,2),
+                        new Rectangle(22,15),
+                        new Rectangle(7,5),
+                        new Rectangle(9,5),
+                        new Rectangle(34,25),
+                        new Rectangle(19,50),
+                        new Rectangle(40,19)
+                )
+        );
+
+        RectangleComparator comparator = new RectangleComparator();
+
+        System.out.println("\n-unsorted rectangleList-");
+        for (Rectangle rectangle : rectangleList) {
+            System.out.println(rectangle);
+        }
         System.out.println();
 
+        rectangleList.sort(comparator);
 
-        Circle doubleSizedCircle = circle.returnDoubleSizedCircle(circle);
+        System.out.println("-sorted rectangleList-");
+        for (Rectangle rectangle : rectangleList) {
+            System.out.println(rectangle);
+        }
 
-        System.out.println("doubleSizedCircle area - " + doubleSizedCircle.getArea());
-        System.out.println("doubleSizedCircle length - " + doubleSizedCircle.getLength());
-        System.out.println();
-
-
-        Circle customSizedCircle = circle.returnCustomSizedCircle(circle, 5);
-
-        System.out.println("customSizedCircle area - " + customSizedCircle.getArea());
-        System.out.println("customSizedCircle length - " + customSizedCircle.getLength());
-        System.out.println();
-
-        System.out.println(circle.validateCircle(new Circle(-10)));
-        System.out.println(circle.validateCircle(new Circle(10)));
 
     }
-
 }
