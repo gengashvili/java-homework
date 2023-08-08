955cc5e9a7e5149814b11cede851980cf9be32ac
giorgi gengashvili <giorgi.gengashvili0@gmail.com>
2023-08-08 16:13:59 +0400

    finish generics homework

--- src/main/java/ge/tbc/testautomation/javaoop/figures/Rectangle.java
+++ src/main/java/ge/tbc/testautomation/javaoop/figures/Rectangle.java
@@ -1,22 +1,37 @@
-//package ge.tbc.testautomation.javaoop.figures;
-//
-//public class Rectangle extends Figure {
-//    private double a;
-//    private double b;
-//
-//    public Rectangle(double a, double b) {
-//        this.a = a;
-//        this.b = b;
-//    }
-//
-//    @Override
-//    public double getArea() {
-//        return a * b;
-//    }
-//    @Override
-//    public double getPerimeter() {
-//        return (a + b) * 2;
-//    }
-//
-//
-//}
+package ge.tbc.testautomation.javaoop.figures;
+
+public class Rectangle extends Figure {
+    private double a;
+    private double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public double getArea() {
+        return a * b;
+    }
+
+    @Override
+    public double getLength() {
+        return 0;
+    }
+
+    @Override
+    public void printPackageName() {
+        String currentPackage = Circle.class.getPackage().getName();
+
+        System.out.println("current class package - " + currentPackage);
+    }
+
+    public double getPerimeter() {
+        return (a + b) * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle {" + " a = " + a + ", b=" + b + " }";
+    }
+}
